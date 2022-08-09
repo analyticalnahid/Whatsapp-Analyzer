fbd63fb37c71c6c3ebb5228225f9c60a7d54cc28
Nahidul Islam <97219701+analyticalnahid@users.noreply.github.com>
2022-08-09 17:49:19 +0600

    Update app.py

--- app.py
+++ app.py
@@ -116,21 +116,4 @@ if uploaded_file is not None:
         plt.xticks(rotation='vertical')
         st.title("Most Common Words")
         st.pyplot(fig)
-        
-## 4th Part
-
-"""    emoji_df = helper.emoji_helper(selected_user, df) 
-        st.title("Emoji Analysis") 
-        
-        col1, col2 = st.columns(2) 
-        
-        with col1:
-            st.dataframe(emoji_df) 
-        
-        with col2:
-            fig, ax = plt.subplots()
-            ax.pie(emoji_df[1].head(), labels=emoji_df[0].head(), autopct="%0.2f")
-            st.pyplot(fig)
-            
-"""            
-                    
+                      
